0d746d2bfae30e2328aa66387e55b389875ae51f
gjh <pp90971658@gmail.com>
2022-06-19 18:45:59 +0900

    [Silver IV] Title: 한수, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/1065. 한수/한수.py
@@ -0,0 +1,9 @@
+n = int(input())
+hansu = 0
+for i in range(1, n+1):
+    arr = list(map(int, str(i)))
+    if i < 100:
+        hansu += 1
+    elif arr[0] - arr[1] == arr[1] - arr[2]:
+        hansu += 1
+print(hansu)
