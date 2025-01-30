40fae4bfef6215620e090c07e1ae37607ac34350
gjh <pp90971658@gmail.com>
2025-01-30 19:31:27 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 96 ms, Memory: 38012 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -18,14 +18,14 @@ def BFS():
             return visited[x]
 
         # 순간이동한 위치가 범위 안에 있고, 방문하지 않은 곳이면
-        if (2*x < 100001) and (visited[2*x] == 0):
+        if (2*x <= 100000) and (visited[2*x] == 0):
             # 0초 소요
             visited[2*x] = visited[x]
             q.append(2*x)
 
         # 걷기
         for i in (x-1, x+1):
-            if (0 <= i < 100001) and (visited[i] == 0):
+            if (0 <= i <= 100000) and (visited[i] == 0):
                 # 1초 소요
                 visited[i] = visited[x] + 1
                 q.append(i)
