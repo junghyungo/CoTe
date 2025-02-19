c2f453f7ca1f65fd425579663a092f49755f2677
gjh <pp90971658@gmail.com>
2025-02-19 14:19:32 +0900

    [Silver V] Title: 다리 놓기, Time: 32 ms, Memory: 34536 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.py
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.py
@@ -1,11 +1,12 @@
-for i in range(int(input())):
-    n,m = map(int, input().split())
-    n = min(n, m-n)
-    a = 1
-    b = 1
-    for i in range(n):
-        a *= m
-        m -= 1
-        b *= n
-        n -= 1
-    print(a//b)
+import sys
+input = sys.stdin.readline
+import math
+
+for _ in range(int(input())):
+    n, m = map(int, input().split())
+    
+    if (n == m):
+        print(1)
+        continue
+    else:
+        print(int(math.factorial(m) / (math.factorial(n) * math.factorial(m-n))))
