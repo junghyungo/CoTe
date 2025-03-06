94e2717e6be142adceea049ad599ea022f277698
gjh <pp90971658@gmail.com>
2025-03-07 02:11:11 +0900

    [Silver II] Title: A → B, Time: 32 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Silver/16953. A → B/A → B.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+a, b = map(int, input().split())
+
+cnt = 1
+
+while (b > a):
+    if (b%10 == 1):
+        b //= 10
+    elif (b%2 == 0):
+        b //= 2
+    else:
+        break
+    cnt += 1
+
+if (a == b):
+    print(cnt)
+else:
+    print(-1)
