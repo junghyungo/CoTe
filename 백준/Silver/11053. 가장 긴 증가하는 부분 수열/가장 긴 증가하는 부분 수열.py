6f80c76674a0eed91c227e98127338589af6c488
gjh <pp90971658@gmail.com>
2025-03-12 21:59:27 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 36 ms, Memory: 34476 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
@@ -1,14 +1,23 @@
+from bisect import bisect_left
 import sys
 input = sys.stdin.readline
 
 n = int(input())
 arr = list(map(int, input().split()))
 
-dp = [1]*n
+# LIS 배열 (실제 LIS를 저장하는 것이 아니라 길이만 관리)
+lis = []
 
-for i in range(n):
-    for j in range(i):
-        if (arr[i] > arr[j]):
-            dp[i] = max(dp[i], dp[j]+1)
+for num in arr:
 
-print(max(dp))
+		# 현재 숫자가 들어갈 위치 탐색
+    pos = bisect_left(lis, num)
+    
+    if (pos == len(lis)):
+		    # 새 숫자가 LIS 배열의 끝에 추가됨
+        lis.append(num)
+    else:
+		    # 기존 값과 교체하여 LIS 길이 유지
+        lis[pos] = num  
+
+print(len(lis))
