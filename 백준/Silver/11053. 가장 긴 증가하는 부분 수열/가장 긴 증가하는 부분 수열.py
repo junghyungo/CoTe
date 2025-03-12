6fcb7303802eabdf19d79a6c7c48f240863f0199
gjh <pp90971658@gmail.com>
2025-03-12 21:53:31 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 128 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
@@ -1,26 +1,14 @@
 import sys
 input = sys.stdin.readline
 
-def longest_increasing_subsequence(n, arr):
-    dp = [1] * n  # 모든 원소는 길이 1의 LIS로 시작
-
-    for i in range(n):
-        for j in range(i):
-            if arr[j] < arr[i]:  # 증가하는 관계일 때
-                dp[i] = max(dp[i], dp[j] + 1)
-
-    return max(dp)  # 가장 긴 부분 수열의 길이 반환
-
 n = int(input())
 arr = list(map(int, input().split()))
 
-# 모든 원소는 길이 1로 시작
-DP = [1]*n
+dp = [1]*n
 
 for i in range(n):
     for j in range(i):
-        # 증가할 때
-        if (arr[j] < arr[i]):
-            DP[i] = max(DP[i], DP[j]+1)
+        if (arr[i] > arr[j]):
+            dp[i] = max(dp[i], dp[j]+1)
 
-print(max(DP))
+print(max(dp))
